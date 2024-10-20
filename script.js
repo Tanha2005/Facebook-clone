4dcebf8a0a0c9f326b9b4e233ed29d776572d067
tanha <tanha2005akter@gmail.com>
2024-10-20 08:44:02 +0600

    dn

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ function settingsMenuToggle(){
 darkBtn.onclick = function(){
     darkBtn.classList.toggle("dark-btn-on");
     document.body.classList.toggle("dark-theme");
+
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme","dark");
+
+    }
+    else{
+        localStorage.setItem("theme","light");
+
+    }
 }
     
     
